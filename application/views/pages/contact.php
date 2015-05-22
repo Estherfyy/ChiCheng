f1fd851334a2291a086b4f5a7b85dd8a8fb8a1d7
guxiaobai <sikuan.gu@gmail.com>
2015-05-22 18:45:07 +0800

    联系我们

---
+++ application/views/pages/contact.php
@@ -0,0 +1,26 @@
+<div class="mid_content2">
+  <div class="caiwu4"> </div>
+	<div class="home_content mid">
+    <div class="h_con_Left">
+			<a href="/contact">
+				<div class="bigtitle">联系我们</div>
+			</a>
+		</div>
+
+		<div class="h_con_Right">
+
+<p>公司地址：上海市自贸区富特北路18号联安大厦5楼</p>
+
+<h3>联系我们</h3>
+<p>
+程  笑（电话）18702159217（邮箱）106245924@qq.com（微信）18702159217<br />
+高媛媛（电话）18817502679 (邮箱)1002859078@qq.com （微信）yuanxi5200<br />
+郭正文（电话）17751384886 (邮箱)575250937@qq.com    (微信)575250937<br />
+</p>
+
+<img src="/images/map.jpg" />
+
+		</div>
+		<div class="clear">&nbsp;</div>
+	</div>
+</div>
